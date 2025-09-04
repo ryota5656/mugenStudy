ca5fa1f04c58c26a123031cba7bd21e244cd4f91
ryota saito <90943612+ryota5656@users.noreply.github.com>
2025-09-05 00:24:54 +0900

    Initial Commit

---
+++ mugenStudy/mugenStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  mugenStudyApp.swift
+//  mugenStudy
+//
+//  Created by ryota.saito on 2025/09/05.
+//
+
+import SwiftUI
+
+@main
+struct mugenStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mugenStudyTests/mugenStudyTests.swift
@@ -0,0 +1,16 @@
+//
+//  mugenStudyTests.swift
+//  mugenStudyTests
+//
+//  Created by ryota.saito on 2025/09/05.
+//
+
+import Testing
+
+struct mugenStudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
